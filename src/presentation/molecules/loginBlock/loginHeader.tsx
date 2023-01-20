5191307b55681c67c1be0b5cf54d884ef3cea869
erentrk01 <erentarak1907@gmail.com>
2023-01-20 14:40:31 +0300

    Animations done

--- src/presentation/molecules/loginBlock/loginHeader.tsx
+++ src/presentation/molecules/loginBlock/loginHeader.tsx
@@ -1,12 +1,16 @@
 import {Box,Heading,Text,Link,} from '@chakra-ui/react'
+import {motion} from 'framer-motion'
 
 
 export const LoginHeader = () => {
 	const VARIANT_COLOR = 'teal'
+	let easing =[0.6, -0.05, 0.01, 0.99]
 
 	return (
 	  <Box textAlign='center'>
-		<Heading>Sign In to Your Account</Heading>
+				<motion.div initial={{opacity:0,scale:0}} animate={{opacity:1,scale:1}} transition={{duration:0.3,ease:easing}}>
+					<Heading>Sign In to Your Account</Heading>
+				</motion.div>
 		<Text>
 		  Or <Link color={`${VARIANT_COLOR}.500`}>start your 14 days trial</Link>
 		</Text>
---
+++ src/services/animation/animation.common.ts
@@ -0,0 +1,1 @@
+export const easing =[0.6, -0.05, 0.01, 0.99]
