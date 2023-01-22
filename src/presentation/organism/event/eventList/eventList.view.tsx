dbefcbd393c58566a9e6180dd0746a06e83a9694
erentrk01 <erentarak1907@gmail.com>
2023-01-22 20:45:38 +0300

    Event card designed

--- src/presentation/organism/event/eventList/eventList.view.tsx
+++ src/presentation/organism/event/eventList/eventList.view.tsx
@@ -8,59 +8,80 @@ import {
 	UnorderedList,
 	Card,
 	CardHeader,
+	CardBody,
 	VStack,
 	Text,
+	CardFooter,
   } from '@chakra-ui/react'
-import { IconContext } from "react-icons";
+import { IconBaseProps, IconContext, IconType } from "react-icons";
 import { IoMailOutline } from "react-icons/io5";
+import{IoMdDoneAll} from "react-icons/io"
+import {GrLaunch} from "react-icons/gr"
+
 import { Skeleton, SkeletonCircle, SkeletonText } from '@chakra-ui/react'
 import SkeletonEvents from "../../../molecules/skeletonBlock/skeletonEvents";
 
 
 export const EventList = () => {
-	let isLoaded = false;
-
 	const {getEvents,events} = EventListViewModel();
-	isLoaded = true;
-	console.log(events.length)
+
+
 	useEffect(()=>{
 		getEvents();
 		
 	},[events])
+
+	const detectConditionIcon  = (condition) => {
+		switch (condition) {
+			case "ongoing" :
+				return <GrLaunch/>
+			case "done" || "tamamlandı":
+				return <IoMdDoneAll/>
+			case "waiting":
+				return <IoMailOutline/>
+			default:
+				return <IoMailOutline/>
+		}
+	}
 	
 	
 	return(
 		<VStack >
 		
-		{events.length === 0 &&
-		<SkeletonEvents/>
-		}
-		<List>
+			{events.length === 0 &&
+				<SkeletonEvents/>
+			}
 
+			<List>
 			{
-				
-				
-				
-			events.map((event,i) => {
-                return (
+				events.map((event,i) => {
+					return (
 					
-					<Card mt={5} key={i}>
-						<CardHeader>
-							<IconContext.Provider value={{color:"#14da8f",size:"22px"}}>
-							<IoMailOutline/>
-					</IconContext.Provider>
-					{events.length}
-						</CardHeader>
-                    <ListItem >
-                          <ListIcon color='green.500' />
-					{event.description}
+						<Card mt={5} key={i}>
+							<CardHeader>
+								<IconContext.Provider value={{color:"#14da8f",size:"22px"}}>
+									{
+										detectConditionIcon(event.condition)
+									}
+								</IconContext.Provider>
+								{event.title}
+							</CardHeader>
+							<ListItem >
+								<ListIcon color='green.500' />
+								{}
+							</ListItem>
+							<CardBody>
+								<Text>{event.description}</Text>
+							</CardBody>
+							<CardFooter>
+								<Text>{event.date}</Text>
+							</CardFooter>
+						</Card>
 
-                    </ListItem>
-					</Card>
+					);
+			})}
+			</List>
 
-                );
-            })}
-		</List>
 		</VStack>
 
 	)
