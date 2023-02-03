bd1bab9ab5d6e92add16bfb61d44ce0d7ad1a34b
erentrk01 <erentarak1907@gmail.com>
2023-02-03 18:38:44 +0300

    Eventlist: event card designed

--- src/presentation/organism/event/eventList/eventList.view.tsx
+++ src/presentation/organism/event/eventList/eventList.view.tsx
@@ -12,14 +12,20 @@ import {
 	VStack,
 	Text,
 	CardFooter,
+	Box,
+	HStack,
   } from '@chakra-ui/react'
 import { IconBaseProps, IconContext, IconType } from "react-icons";
 import { IoMailOutline } from "react-icons/io5";
 import{IoMdDoneAll} from "react-icons/io"
-import {GrLaunch} from "react-icons/gr"
+import {MdTipsAndUpdates} from "react-icons/md"
+import {GrLaunch,GrUpdate} from "react-icons/gr"
+import {AiTwotoneDelete} from "react-icons/ai";
+import {SlLike} from "react-icons/sl";
 
 import { Skeleton, SkeletonCircle, SkeletonText } from '@chakra-ui/react'
 import SkeletonEvents from "../../../molecules/skeletonBlock/skeletonEvents";
+
 
 
 export const EventList = () => {
@@ -57,11 +63,12 @@ export const EventList = () => {
 				events.map((event,i) => {
 					return (
 					
-						<Card mt={5} key={i}>
+						<Card mt={5} key={i} borderRadius={12}>
 							<CardHeader>
 								<IconContext.Provider value={{color:"#14da8f",size:"22px"}}>
 									{
-										detectConditionIcon(event.condition)
+										
+									detectConditionIcon(event.condition)
 									}
 								</IconContext.Provider>
 								{event.title}
@@ -71,9 +78,24 @@ export const EventList = () => {
 								{}
 							</ListItem>
 							<CardBody>
+
+								
+								<Box bg='green.500' borderRadius={12} padding={3}>
 								<Text>{event.description}</Text>
+								</Box>
+								<HStack  borderRadius={12} padding={3}>
+									<IconContext.Provider value={{color:"#14da8f",size:"22px"}}>
+										<AiTwotoneDelete/>
+									</IconContext.Provider>
+									<IconContext.Provider value={{color:"#14da8f",size:"22px"}}>
+										<SlLike/>
+									</IconContext.Provider>
+									<IconContext.Provider value={{color:"#14da8f",size:"22px"}}>
+									 <MdTipsAndUpdates/>
+									</IconContext.Provider>
+								</HStack>
 							</CardBody>
-							<CardFooter>
+							<CardFooter >
 								<Text>{event.date}</Text>
 							</CardFooter>
 						</Card>
