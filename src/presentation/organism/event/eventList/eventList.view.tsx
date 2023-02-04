c4c62bc7c9d925999aab7d7342238cfccf5ad00b
erentrk01 <erentarak1907@gmail.com>
2023-02-04 16:24:27 +0300

    Event Card:EventBody and EventFooter done

--- src/presentation/organism/event/eventList/eventList.view.tsx
+++ src/presentation/organism/event/eventList/eventList.view.tsx
@@ -6,8 +6,6 @@ import {
 	ListIcon,
 	Card,
 	CardHeader,
-	CardBody,
-	VStack,
 	Text,
 	CardFooter,
 	Box,
@@ -19,17 +17,15 @@ import {
 	AlertDialogHeader,
 	AlertDialogBody,
 	AlertDialog,
-	Collapse,
-	SimpleGrid
+	SimpleGrid,
+	VStack
   } from '@chakra-ui/react'
 import { IconBaseProps, IconContext, IconType } from "react-icons";
 import { IoMailOutline } from "react-icons/io5";
 import{IoMdDoneAll} from "react-icons/io"
-import {MdTipsAndUpdates} from "react-icons/md"
+
 import {GrLaunch,GrUpdate} from "react-icons/gr"
-import {AiTwotoneDelete} from "react-icons/ai";
-import {SlLike} from "react-icons/sl";
-import {FcShare} from "react-icons/fc";
+;
 
 import SkeletonEvents from "../../../molecules/skeletonBlock/skeletonEvents";
 
@@ -37,6 +33,8 @@ import SkeletonEvents from "../../../molecules/skeletonBlock/skeletonEvents";
 import { ReactComponent as Home} from '../../../../assets/building.svg'
 import { useDisclosure } from "@chakra-ui/react";
 import { useRef} from "react";
+import EventBody from "../../../molecules/eventCard/eventBody";
+import EventFooter from "../../../molecules/eventCard/eventFooter";
 
 
 export const EventList = () => {
@@ -122,62 +120,15 @@ export const EventList = () => {
 										height="100px"/>
 								</HStack>
 								<Box width="300px" bg='orange.300' borderRadius={12} padding={2}>
-								
 								<Text>{event.title}</Text>
 								</Box>
-
 							</CardHeader>
 							<ListItem >
 								<ListIcon color='green.500' />
 								{}
-								
-								
 							</ListItem>
-							<CardBody>
-								
-								<Box bg="gray.400" borderRadius={12} padding={2}>
-									<Text>{event.functionalArea}</Text>
-								</Box>
-								<Box mt={1} bg='green.600' borderRadius={12} padding={2}>
-								<Collapse startingHeight={20} in={show}>
-									{event.description}
-      							</Collapse>
-      							<Button size='sm' onClick={handleToggle} mt='1rem'>
-       						 		Show {show ? 'Less' : 'More'}
-      							</Button>
-								<Text></Text>
-								</Box>
-								<Box mt={1} bg='green.500' borderRadius={12} padding={2}>
-								<Text>{event.serviceContactPhone}</Text>
-								</Box>
-								<Text>{event.date}</Text>
-							</CardBody>
-							<CardFooter >
-								<HStack justifyContent={"space-between"}>
-								<HStack  											
-									borderRadius={12} padding={3}>
-										<Button onClick={onOpen} >
-											<IconContext.Provider value={{color:"#14da8f",size:"22px"}}>
-												<AiTwotoneDelete/>
-											</IconContext.Provider>
-										</Button>
-	
-									<IconContext.Provider
-										value={{color:"#14da8f",size:"22px"}}>
-										<SlLike
-										/>
-									</IconContext.Provider>
-
-									<IconContext.Provider value={{color:"#14da8f",size:"22px"}}>
-									 <MdTipsAndUpdates/>
-									</IconContext.Provider>
-								</HStack>
-								<IconContext.Provider value={{color:"#14da8f",size:"22px"}}>
-									 <FcShare/>
-									</IconContext.Provider>
-								</HStack>
-
-							</CardFooter>
+							<EventBody event={event}/>
+							<EventFooter onOpen={onOpen}/>
 						</Card>
 
 					);
@@ -187,6 +138,5 @@ export const EventList = () => {
 			</SimpleGrid>
 
 		</VStack>
-
 	)
 }
