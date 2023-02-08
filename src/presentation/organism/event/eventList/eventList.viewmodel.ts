7b10ea83b05a8a457bc4f0eedf0b3be879e53ff4
erentrk01 <erentarak1907@gmail.com>
2023-02-08 18:05:04 +0300

    FetchedEvents integrated into events state

--- src/presentation/organism/event/eventList/eventList.viewmodel.ts
+++ src/presentation/organism/event/eventList/eventList.viewmodel.ts
@@ -3,9 +3,12 @@ import EventAPIDataSourceImpl from "../../../../data/datasource/api/eventsApi.da
 import { EventsRepositoryImpl } from "../../../../data/repository/events.repository.impl";
 import { Event } from "../../../../domain/model/event";
 import { GetEvents } from "../../../../domain/usecases/event/getEvents";
+import { useAppDispatch } from "../../../../store";
 
 
 export  const EventListViewModel = () => {
+	const dispatch = useAppDispatch();
+	
 	const [events, setEvents] = useState<Event[]>([]);
 	const UseCase = new GetEvents(
 		new EventsRepositoryImpl(new EventAPIDataSourceImpl())
@@ -13,6 +16,7 @@ export  const EventListViewModel = () => {
 
 	const getEvents = async (buildingId:string) => {
 			setEvents(await UseCase.invoke(buildingId))
+
 	}
 
 	return{
