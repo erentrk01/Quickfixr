c9e3a09ffa67034d8a5eee6d7c18eb2b2e180524
erentrk01 <erentarak1907@gmail.com>
2023-02-10 12:20:31 +0300

    Store: Redux Saga Config Setting Up

--- src/store.ts
+++ src/store.ts
@@ -4,12 +4,15 @@ import authSlice from "./domain/usecases/authenticate/login/login.usecase";
 import eventSlice from "./domain/usecases/event/eventSlice";
 import { TypedUseSelectorHook, useDispatch,useSelector } from "react-redux";
 
+import createSagaMiddleware from '@redux-saga/core';
+const saga = createSagaMiddleware()
 
 export const store=  configureStore({
 	  reducer: {
 		auth:authSlice.reducer,
 		event:eventSlice.reducer
-	  }
+	  },
+	  middleware:[saga]
 })
 
 export const useAppDispatch:() => typeof store.dispatch= useDispatch;
