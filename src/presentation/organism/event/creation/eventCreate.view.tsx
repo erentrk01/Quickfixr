c1bd764bd56a3b400864c29ebd7c65d28f00c4cd
Eren Tarak <erentarak1907@gmail.com>
2023-02-28 13:22:09 +0300

    Dropzone import removed

--- src/presentation/organism/event/creation/eventCreate.view.tsx
+++ src/presentation/organism/event/creation/eventCreate.view.tsx
@@ -7,7 +7,7 @@ import { addPostToDB } from "../../../../domain/usecases/event/eventSlice";
 
 import { useNavigate} from 'react-router-dom';
 import {resetEventCreationState} from "../../../../domain/usecases/event/eventSlice";
-import Dropzone from "../dropzone/dropzone";
+
 
 const EventCreateView = ({isOpen,onClose}) => {
 
